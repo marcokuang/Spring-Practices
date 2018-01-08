a98dc2341d993cc6e048203a70a1209713506d5c
Marco Kuang <marcojkuang@gmail.com>
2018-01-07 23:13:44 -0800

    finished exercise 3: using environmental variables

--- src/main/java/org/marco/common/config/AppConfig.java
+++ src/main/java/org/marco/common/config/AppConfig.java
@@ -8,15 +8,37 @@ import org.marco.common.service.OrderService;
 import org.marco.common.service.impl.InventoryServiceImpl;
 import org.marco.common.service.impl.OrderServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Import(DataConfig.class)
+@PropertySource("classpath:/application.properties")
 public class AppConfig {
+	
+	@Value("${gretting.text}")
+	private String greetingText;
+	
+	public class Worker{
+		private String text;
+		public Worker(String text) {
+			this.text = text;
+		}
+		
+		public void execute() {
+			System.out.println("Hello " + text);
+		}
+	}
+	
+	@Bean
+	public Worker worker() {
+		return new Worker(greetingText);
+	}
 	
 	@Autowired
 	private CustomerRepository customerRepository;
@@ -44,6 +66,9 @@ public class AppConfig {
 		OrderService orderService = context.getBean(OrderService.class);
 		
 		String output = (orderService == null ? "not ok" : "ok");
+		
+		Worker worker = context.getBean(Worker.class);
+		worker.execute();
 		
 		System.out.println(output);
 	}
